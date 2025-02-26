7a00634704bbb6a42626d00c5e4e3ccee0970095
Joseph Kennedy <joekentafir@gmail.com>
2025-02-26 16:15:38 +0300

    made few changes

--- thesizzledmunch/src/Components/Navbar.js
+++ thesizzledmunch/src/Components/Navbar.js
@@ -4,7 +4,29 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div>
-     
+
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="navbar">The Sizzled Munch</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="home">Enqueries</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            </form>
+          </div>
+        </div>
+      </nav>
+
     </div>
   )
 }
