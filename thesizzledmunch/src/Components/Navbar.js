30ed65595318ac3647bdee8c723ae68178abc874
Joseph Kennedy <joekentafir@gmail.com>
2025-02-27 22:35:51 +0300

    adds new css

--- thesizzledmunch/src/Components/Navbar.js
+++ thesizzledmunch/src/Components/Navbar.js
@@ -1,34 +1,38 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <div>
-
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <a className="navbar-brand" href="navbar">The Sizzled Munch</a>
+
+          {/* Centered Search Bar */}
+          <div className="search-container">
+            <form className="d-flex" role="search">
+              <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+            </form>
+          </div>
+
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="home">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="home">Enqueries</a>
+                <a className="nav-link active" aria-current="page" href="home">Enquiries</a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            </form>
           </div>
         </div>
       </nav>
-
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
