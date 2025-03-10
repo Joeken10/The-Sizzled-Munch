7cadbd11f9be13522ffac3a5ca72c3afd86f9343
Joseph Kennedy <joekentafir@gmail.com>
2025-03-10 23:24:05 +0300

    created home page

---
+++ thesizzledmunch/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to The Sizzled Munch</h1>
+            <p>Your go-to spot for delicious and sizzling meals. Order now and enjoy!</p>
+            <button className="order-btn">Order Now</button>
+        </div>
+    );
+};
+
+export default Home;
