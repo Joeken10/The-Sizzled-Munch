964036137cb424e32227f09163fee081220a3f57
Joseph Kennedy <joekentafir@gmail.com>
2025-03-15 13:10:26 +0300

    rename and start creating a home page

--- thesizzledmunch/src/App.js
+++ thesizzledmunch/src/App.js
@@ -1,32 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
-import Home from "./Components/Home";
-import Checkout from "./Components/Checkout";
-import Receipt from "./Components/Receipt";
-import Profile from "./Components/Profile";
-
-const Navbar = () => (
-    <nav className="navbar">
-        <NavLink to="/" className="nav-link">Home</NavLink>
-        <NavLink to="/checkout" className="nav-link">Checkout</NavLink>
-        <NavLink to="/receipt" className="nav-link">Receipt</NavLink>
-        <NavLink to="/profile" className="nav-link">Profile</NavLink>
-    </nav>
-);
+import React from 'react'
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <div className="container">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                    <Route path="/receipt" element={<Receipt />} />
-                    <Route path="/profile" element={<Profile />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
