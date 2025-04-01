93fa54130505e4587bf65708efd3c9bc13baea38
Joseph Kennedy <joekentafir@gmail.com>
2025-04-01 23:30:09 +0300

    add to cart button created

--- thesizzledmunch/src/Components/Navbar.js
+++ thesizzledmunch/src/Components/Navbar.js
@@ -22,6 +22,7 @@ function Navbar() {
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                 <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
+            <button>add to cart</button>
           </div>
         </div>
       </nav>
