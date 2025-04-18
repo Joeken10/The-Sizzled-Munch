c4165d12b6ee124961ce7eb4e794bafae444f1f6
Joseph Kennedy <joekentafir@gmail.com>
2025-04-18 13:05:57 +0300

    made very many deletions

--- thesizzledmunch/src/App.js
+++ thesizzledmunch/src/App.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import Home from './Components/files/Home'
-import Footer from './Components/Footer'
-import Navbar from './Components/Navbar'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-     <Navbar />
-      <Home /> 
-      <Footer />
-      
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
