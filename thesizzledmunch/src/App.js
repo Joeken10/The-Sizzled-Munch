7c3c85962cb5ad01d90ba8729c0aea63a77124cc
Joseph Kennedy <joekentafir@gmail.com>
2025-04-24 21:14:20 +0300

    add footer

--- thesizzledmunch/src/App.js
+++ thesizzledmunch/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Navbar />
       <h1>The Sizzled Munch</h1>
+      <Footer />
     </div>
   )
 }
-
 export default App
