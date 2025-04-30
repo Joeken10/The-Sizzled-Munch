5bad1b5f6195e37f8e86fa65800c47636c656b03
Joseph Kennedy <joekentafir@gmail.com>
2025-04-30 13:21:22 +0300

    made some few deletion and changes

--- thesizzledmunch/src/components/MenuItems.js
+++ thesizzledmunch/src/components/MenuItems.js
@@ -6,11 +6,12 @@ function MenuItems() {
   return (
     <div>
       <div className="card" style={{width: '18rem'}}>
-        <img src={menuAlbum} className="card-img-top" alt="food1"/>
+        <img src={menuAlbum} className="card-img-top" alt="Classic American Burger with Fried Egg"/>
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="ingridients" className="btn btn-primary">Ingridients</a>
+            <h5 className="card-title">itemName</h5>
+            <h6 className="card-title">price</h6>
+            <p className="card-text">extras</p>
+            <a href="ingridients" className="btn btn-primary">description</a>
           </div>
       </div>
       <div className="card" style={{width: '18rem'}}>
