c1f4884c3afa925dfa07226606d7adca1e434483
Joseph Kennedy <joekentafir@gmail.com>
2025-04-30 12:24:14 +0300

    adds the cards

--- thesizzledmunch/src/components/MenuItems.js
+++ thesizzledmunch/src/components/MenuItems.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import "./MenuItems.css"
+import menuAlbum from '../images/Burger Taste.jpeg';
 
 function MenuItems() {
   return (
     <div>
-      <div class="card" style={{width: '18rem'}}>
-        <img src="..." class="card-img-top" alt="food1"/>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="ingridients" class="btn btn-primary">Ingridients</a>
+      <div className="card" style={{width: '18rem'}}>
+        <img src={menuAlbum} className="card-img-top" alt="food1"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="ingridients" className="btn btn-primary">Ingridients</a>
+          </div>
+      </div>
+      <div className="card" style={{width: '18rem'}}>
+        <img src={menuAlbum} className="card-img-top" alt="food1"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="ingridients" className="btn btn-primary">Ingridients</a>
           </div>
       </div>
     </div>
