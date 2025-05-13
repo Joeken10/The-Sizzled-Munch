dc753f017a5057a5bca32bf9bd95c2ccbe236166
Joseph Kennedy <joekentafir@gmail.com>
2025-05-13 05:05:12 +0300

    import of BrowserRouter in my app.js

--- thesizzledmunch/src/App.js
+++ thesizzledmunch/src/App.js
@@ -2,10 +2,18 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import MenuList from './components/MenuList'
+import {BrowserRuter as Router, Routes, BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="" element="" />
+        </Routes>
+        </Router>
+      </BrowserRouter>
       <Navbar />
       <MenuList />
       <Footer />
