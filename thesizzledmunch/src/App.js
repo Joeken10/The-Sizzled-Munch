ed999f926924d6c872bb982742aef767b32c9fb7
Joseph Kennedy <joekentafir@gmail.com>
2025-05-14 00:03:26 +0300

    moves Navbar and MenuList to HomePages.js

--- thesizzledmunch/src/App.js
+++ thesizzledmunch/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import MenuList from './components/MenuList'
+
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePages from './pages/HomePages';
 
@@ -13,8 +13,7 @@ function App() {
           <Route path="/" element={<HomePages/>} />
         </Routes>
       </BrowserRouter>
-      <Navbar />
-      <MenuList />
+      
       <Footer />
     </div>
     
--- thesizzledmunch/src/pages/HomePages.js
+++ thesizzledmunch/src/pages/HomePages.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import MenuList from '../components/MenuList'
+import Navbar from '../components/Navbar'
 
 function HomePages() {
   return (
     <div>
+       <Navbar />
       <MenuList />
+     
     </div>
   )
 }
